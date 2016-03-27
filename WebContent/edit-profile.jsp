1df2366c01dff328409cfa56f58b0ff0f9e574a0
Alex Jones <ajones35@uga.edu>
2016-03-27 18:36:30 -0400

    Update user
    Added new DB helpers and servlets to update existing user

--- WebContent/edit-profile.jsp
+++ WebContent/edit-profile.jsp
@@ -15,22 +15,32 @@
 	<!-- UPDATED CONTENT added <p> tags, and took out all of the <br> tags -->
 	<h1>Edit Profile</h1>
 	<div id="new-user-form">
-	<form>
-  		<p>First Name:
-		<input type="text" name="firstname"></p>
-  		<p>Last Name:
-  		<input type="text" name="lastname"></p>
-		<p>Gender:
-  		<select id="gender">
+	<form action="UpdateUser" method="post">
+	<p>Email address associated with account:
+  		<input type="text" name="email"></p>
+  		<p>Updated First Name:
+		<input type="text" name="firstName"></p>
+  		<p>Updated Last Name:
+  		<input type="text" name="lastName"></p>
+		<p>Updated Gender:
+  		<select id="gender" name="gender">
 			<option>Male</option>
 			<option>Female</option>
 		</select></p>
-		<p>Email:
-  		<input type="text" name="email"></p>
-		<p>Handicap:
+		<p>Updated Handicap:
   		<input type="text" name="handicap"></p>
+		<p>Updated Password:
+  		<input type="text" name="password"></p>
+		<p>Password<br>Confirmation:
+  		<input type="text" name="password"></p>
 		<div id="buttons">
+		<br>
+		<br>
 		<input type="submit" value="Log In">
+		</div>
+	</form>
+	<form action="index.jsp">
+		<div id="buttons">
 		<input type="submit" value="Back">
 		</div>
 	</form> 
---
+++ src/controllers/UpdateUser.java
@@ -0,0 +1,83 @@
+package controllers;
+
+
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import utilities.PasswordService;
+import model.User;
+import dbhelpers.AddUser;
+import dbhelpers.UserDBHelper;
+import dbhelpers.UpdateUserHelper;
+
+
+/**
+ * Servlet implementation class AddUser
+ */
+@WebServlet(description = "Update's a user and their information to the database", urlPatterns = { "/UpdateUser" })
+public class UpdateUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateUser() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String gender = request.getParameter("gender");
+		String password = request.getParameter("password");
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String email = request.getParameter("email");
+
+		float handicapIndex = Float.valueOf(request.getParameter("handicap"));
+		int avgScoreGross = 0;
+		int avgScoreNet = 0;
+
+		PasswordService pws = new PasswordService();
+		String encryptedPass = pws.encrypt(password);
+
+		//Updated next line to use new default constructor to create object
+		User user = new User();
+		user.setGender(gender);
+		user.setPassword(encryptedPass);
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		user.setEmail(email);
+		//added three lines to update three new fields
+		user.setGolferHandicapIndex(handicapIndex);
+		user.setGolferAvgScoreGross(avgScoreGross);
+		user.setGolferAvgScoreNet(avgScoreNet);	
+
+		UpdateUserHelper uq = new UpdateUserHelper("tomcatdb","root","bu11fr0g");
+		uq.doUpdate(user);
+
+		String url = "login.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+		dispatcher.forward(request,response);
+		
+	}
+	}
